29637dd56c9d30efd5cc381b279708db7e9df806
Sonu <sonu3660@gmail.com>
2020-05-26 21:32:09 +0530

    ready to deploy on heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/contacts_list_db');
+// mongoose.connect('mongodb://localhost/contacts_list_db');
+mongoose.connect('mongodb+srv://sonu:sonu@free-cluster-t4e4s.mongodb.net/Todo?retryWrites=true&w=majority');
+
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error connection to db'));
 db.once('open',function(){
