bccd2814b34d9e542740e334e1bd53254c7ec9ce
Sonu <sonu3660@gmail.com>
2020-07-15 19:05:19 +0530

    Edit database name

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // mongoose.connect('mongodb://localhost/contacts_list_db');
-mongoose.connect('mongodb+srv://sonu:sonu@free-cluster-t4e4s.mongodb.net/Todo?retryWrites=true&w=majority');
+mongoose.connect(
+   "mongodb+srv://sonu:sonu@free-cluster-t4e4s.mongodb.net/Contact_List?retryWrites=true&w=majority"
+);
 
-const db=mongoose.connection;
-db.on('error',console.error.bind(console,'error connection to db'));
-db.once('open',function(){
-    console.log('Successfully connect to the database');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "error connection to db"));
+db.once("open", function () {
+   console.log("Successfully connect to the database");
 });
